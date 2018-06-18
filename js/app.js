50b785708bc2b6d9696b44302f3bc92eade8314b
Paul Braddick <paulbraddick@hotmail.com>
2018-06-18 11:45:56 +0100

    Added move counter / star rating reset on loadup + background styling

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const deck = document.getElementById('deck');
 let moves = 0;
 let counter = document.querySelector('.moves');
 
-const stars = document.querySelectorAll('star');
+const rating = document.getElementById('rating');
 let starList = document.querySelectorAll('.stars li');
 
 let matched = document.getElementsByClassName('matched');
@@ -39,4 +39,19 @@ function start() {
         });
         cards[i].classList.remove("open", "match", "disabled");
     }
-}
+
+    moves = 0;
+    counter.innerHTML = moves;
+ 
+    for (var i= 1; i < starList.length; i++){
+        starList[i].classList.toggle('star-fill');
+        starList[i].classList.toggle('star-unfill');
+    }
+
+    var displayCard = function (){
+    this.classList.toggle("open");
+    this.classList.toggle("face-down");
+    this.classList.toggle("disabled");
+	}
+};
+

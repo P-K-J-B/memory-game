a0dd832a4f4153eed7fd76609443bb69fb838db8
Paul Braddick <paulbraddick@hotmail.com>
2018-06-18 15:40:43 +0100

    Added move counter, star rating & restart button functionality

--- js/app.js
+++ js/app.js
@@ -3,18 +3,20 @@ let cards = [...card];
 
 const deck = document.getElementById('deck');
 
-let moves = 0;
-let counter = document.querySelector('.moves');
-
 const rating = document.getElementById('rating');
 let starList = document.querySelectorAll('.stars li');
 
+let moves = 0;
+let counter = document.querySelector('.moves');
+
+const restart = document.getElementById('restart');
+
 let matchedCard = document.getElementsByClassName('matched');
 
-var openedCards = [];
+let openedCards = [];
 
 function shuffle(array) {
-	var indexNumber = array.length, tempNumber, randomNumber;
+	let indexNumber = array.length, tempNumber, randomNumber;
 	
 	while (indexNumber !== 0) {
 		randomNumber = Math.floor(Math.random()*indexNumber)
@@ -33,19 +35,20 @@ function start() {
 	cards = shuffle(cards);
 
     for (var i = 0; i < cards.length; i++) {
-        deck.innerHTML = "";
+        deck.innerHTML = '';
         [].forEach.call(cards, function(item) {
             deck.appendChild(item);
         });
-        cards[i].classList.remove("open", "match", "disabled");
-    }
+        cards[i].classList.remove('open', 'matched', 'disabled');
+        cards[i].classList.add('face-down');
+    };
 
     moves = 0;
     counter.innerHTML = moves;
  
-    for (var i= 1; i < starList.length; i++) {
-        starList[i].classList.toggle('star-fill');
-        starList[i].classList.toggle('star-unfill');
+    for (var i= 0; i < starList.length; i++) {
+        starList[i].classList.add('star-fill');
+        starList[i].classList.remove('star-unfill');
     };
 };
 
@@ -59,11 +62,12 @@ function cardOpen() {
 	openedCards.push(this);
 	var length = openedCards.length;
 	if(length === 2){
+		moveCounter();
 		if(openedCards[0].type === openedCards[1].type){
 			matched();
 		} else {
 			unmatched();
-		}
+		};
 	};
 };
 
@@ -101,13 +105,28 @@ function enable() {
    		card.classList.remove('disabled');
    		for (let i = 0; i < matchedCard.length; i++) {
    			matchedCard[i].classList.add('disabled');
-   		}
+   		};
    	});
 };
 
+function moveCounter() {
+	moves++;
+	counter.innerHTML = moves;
+	if (moves > 8 && moves < 12) {
+		starList[2].classList.remove('star-fill');
+		starList[2].classList.add('star-unfill');
+	} else if (moves > 13) {
+		starList[1].classList.remove('star-fill');
+		starList[1].classList.add('star-unfill');
+	};
+};
 
 for (let i = 0; i < cards.length; i++) {
 	card = cards[i];
 	card.addEventListener('click', displayCard);
 	card.addEventListener('click', cardOpen);
 };
+
+restart.addEventListener('click', function() {
+	start();
+});

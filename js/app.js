9350c3e8629f44bb32b0837efbdc55c41d25d538
Paul Braddick <paulbraddick@hotmail.com>
2018-06-18 14:50:12 +0100

    Added functionality for matching / unmatching cards

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ let counter = document.querySelector('.moves');
 const rating = document.getElementById('rating');
 let starList = document.querySelectorAll('.stars li');
 
-let matched = document.getElementsByClassName('matched');
+let matchedCard = document.getElementsByClassName('matched');
 
 var openedCards = [];
 
@@ -22,7 +22,7 @@ function shuffle(array) {
 		tempNumber = array[indexNumber];
 		array[indexNumber] = array[randomNumber];
 		array[randomNumber] = tempNumber;
-	}
+	};
 
 	return array;
 };
@@ -32,7 +32,7 @@ document.body.onload = start();
 function start() {
 	cards = shuffle(cards);
 
-    for (var i = 0; i < cards.length; i++){
+    for (var i = 0; i < cards.length; i++) {
         deck.innerHTML = "";
         [].forEach.call(cards, function(item) {
             deck.appendChild(item);
@@ -43,15 +43,71 @@ function start() {
     moves = 0;
     counter.innerHTML = moves;
  
-    for (var i= 1; i < starList.length; i++){
+    for (var i= 1; i < starList.length; i++) {
         starList[i].classList.toggle('star-fill');
         starList[i].classList.toggle('star-unfill');
-    }
-
-    var displayCard = function (){
-    this.classList.toggle("open");
-    this.classList.toggle("face-down");
-    this.classList.toggle("disabled");
-	}
+    };
 };
 
+const displayCard = function() {
+	this.classList.toggle('face-down');
+	this.classList.toggle('open');
+	this.classList.toggle('disabled');
+};
+
+function cardOpen() {
+	openedCards.push(this);
+	var length = openedCards.length;
+	if(length === 2){
+		if(openedCards[0].type === openedCards[1].type){
+			matched();
+		} else {
+			unmatched();
+		}
+	};
+};
+
+function matched() {
+	openedCards[0].classList.add('matched', 'disabled');
+	openedCards[1].classList.add('matched', 'disabled');
+	openedCards[0].classList.remove('open');
+    openedCards[1].classList.remove('open');
+    openedCards = [];
+};
+
+function unmatched() {
+	openedCards[0].classList.add('unmatched');
+	openedCards[1].classList.add('unmatched');
+	disable();
+	setTimeout(function() {
+		openedCards[0].classList.add('face-down');
+		openedCards[1].classList.add('face-down');
+		openedCards[0].classList.remove('open', 'unmatched');
+    	openedCards[1].classList.remove('open', 'unmatched');
+    	enable();
+    	openedCards = [];
+	}, 1000);
+};
+
+function disable() {
+	Array.prototype.filter.call(cards, function(card){
+   		card.classList.add('disabled');
+   		}
+	);
+};
+
+function enable() {
+	Array.prototype.filter.call(cards, function(card){
+   		card.classList.remove('disabled');
+   		for (let i = 0; i < matchedCard.length; i++) {
+   			matchedCard[i].classList.add('disabled');
+   		}
+   	});
+};
+
+
+for (let i = 0; i < cards.length; i++) {
+	card = cards[i];
+	card.addEventListener('click', displayCard);
+	card.addEventListener('click', cardOpen);
+};
